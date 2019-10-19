c347a8d9f27e670d4175b52ba717dbb8ec3d560c
Alex Suthammanont <asuthamm@gmail.com>
2019-10-19 13:39:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,22 @@ def monopoly_with_third_tier
       }, #rent
       :names=>
       {
-  
+        :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline=>
+      {
+        "mortgage_value"=>"$800"
+      }
       } #names
     } #railroads
   }
